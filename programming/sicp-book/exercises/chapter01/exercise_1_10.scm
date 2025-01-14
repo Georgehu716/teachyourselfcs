6271d01c9dd2ce6f10e1698ed33148ce6ff9d4b8
georgehu <2992786775@qq.com>
2025-01-14 15:00:06 +0800

    update exercise 1.10

--- programming/sicp-book/exercises/chapter01/exercise_1_10.scm
+++ programming/sicp-book/exercises/chapter01/exercise_1_10.scm
@@ -28,6 +28,7 @@
 ;; 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
 ;; 2^10
 ;; 1024
+(A 1 3)
 
 
 (A 2 4)
@@ -49,19 +50,35 @@
 
 
 (A 3 3)
+;; (A 2 (A 3 2))
+;; (A 2 (A 2 (A 3 1)))
+;; (A 2 (A 2 2))
+;; (A 2 (A 1 (A 2 1)))
+;; (A 2 (A 1 2))
+;; (A 2 (A 0 (A 1 1)))
+;; (A 2 (A 0 2))
+;; (A 2 (* 2 2))
+;; (A 2 4)
+;; (2 ^ (2 ^ (4)))
+;; 65536
+
 
 
 ;; Consider the following procedures, where A is the procedure defined above:
 
 (define (f n) (A 0 n)) ; f(n) = 2 * n
 (define (g n) (A 1 n)) ; g(n) = 2 ^ n
-(define (h n) (A 2 n)) ; h(n) = 2 ^ (2 ^ n)
+(define (h n) (A 2 n)) ; h(n) = 2 ^ 2 ^ ... ^ 2, n 个
+;; (A 2 3) = 2 ^ 2 ^ 2 = 16
+;; (A 2 4) = 2 ^ 2 ^ 2 ^ 2 = 2 ^ 16 = 65536
+;; (A 2 5) = 2 ^ 2 ^ 2 ^ 2 ^ 2 = 2 ^ 65536
+(A 2 5)
 (define (k n) (* 5 n n))
 
-(f 3)
-(g 3)
-(h 3)
-(h 4)
+(f 3) ; 2 * 3 = 6
+(g 3) ; 2 ^ 3 = 8
+(h 3) ; 2 ^ 2 ^ 2 = 16
+(k 4) ; 5 * 4 * 4 = 80
 
 
 ;; Give concise mathematical definitions for the functions computed by the procedures f, g, and h for positive integer values of n. For example, (k n) computes 5*n^2.
